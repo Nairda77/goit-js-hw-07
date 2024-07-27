b4a724d3fb6aaae85c85f9b68d62aa733f173f61
Nairda77 <148474127+Nairda77@users.noreply.github.com>
2024-07-27 20:49:48 +0300

    update 2.6

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -3,23 +3,28 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(".gallery");
 
-galleryItems.forEach((item) => {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
+function functieGalerie(galleryItems) {
+  galleryItems.forEach((item) => {
+    const galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery__item");
 
-  const link = document.createElement("a");
-  link.href = item.original;
-  link.classList.add("gallery__link");
+    const link = document.createElement("a");
+    link.href = item.original;
+    link.classList.add("gallery__link");
 
-  const image = document.createElement("img");
-  image.src = item.preview;
-  image.alt = item.description;
-  image.classList.add("gallery__image");
+    const image = document.createElement("img");
+    image.src = item.preview;
+    image.alt = item.description;
+    image.classList.add("gallery__image");
 
-  link.appendChild(image);
-  galleryItem.appendChild(link);
-  gallery.appendChild(galleryItem);
-});
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    gallery.appendChild(galleryItem);
+  });
+}
+
+functieGalerie(galleryItems);
+
 document.addEventListener("DOMContentLoaded", function () {
   const lightbox = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
